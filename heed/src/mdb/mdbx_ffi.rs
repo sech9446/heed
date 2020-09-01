9a10c3ebbabca8bdf98dcf0609cd26bfa0b695f8
Eunchul <eunchul.dev@gmail.com>
2020-09-01 21:55:05 +0900

    mdbx ffi::cursor_op::MDB_PREV

--- heed/src/mdb/mdbx_ffi.rs
+++ heed/src/mdb/mdbx_ffi.rs
@@ -45,6 +45,7 @@ pub mod cursor_op {
     pub const MDB_LAST: MDBX_cursor_op = MDBX_cursor_op::MDBX_LAST;
     pub const MDB_SET_RANGE: MDBX_cursor_op = MDBX_cursor_op::MDBX_SET_RANGE;
     pub const MDB_NEXT: MDBX_cursor_op = MDBX_cursor_op::MDBX_NEXT;
+    pub const MDB_PREV: MDBX_cursor_op = MDBX_cursor_op::MDBX_PREV;
 }
 
 pub unsafe fn into_val(value: &[u8]) -> ffi::MDBX_val {
